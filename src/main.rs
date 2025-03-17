12e141ce8fd5917d384b178d21ab72a49945320d
yannsugi <yannsugi966@gmail.com>
2025-03-17 15:49:08 +0900

    main.rsのstd::env::argsのindexを定数化し、可読性を向上

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use sysctl_conf_parser::parse_sysctl_from_path;
 
+const EXPECTED_ARG_COUNT: usize = 2;
+const ERROR_EXIT_CODE: i32 = 1;
+const PROGRAM_NAME_INDEX: usize = 0;
+const FILEPATH_ARG_INDEX: usize = 1;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Usage: {} <path_to_sysctl.conf>", args[0]);
-        std::process::exit(1);
+    if args.len() != EXPECTED_ARG_COUNT {
+        eprintln!("Usage: {} <path_to_sysctl.conf>", args[PROGRAM_NAME_INDEX]);
+        std::process::exit(ERROR_EXIT_CODE);
     }
-    let filename = &args[1];
+    let filename = &args[FILEPATH_ARG_INDEX];
 
     match parse_sysctl_from_path(filename) {
         Ok(config) => println!("Parsed config: {:?}", config),
